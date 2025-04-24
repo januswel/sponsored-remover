9f62a3a3a79b6983983961573cf3dcbc421dbdc9
Takagi Kensuke <janus.wel.3@gmail.com>
2025-04-25 08:58:01 +0900

    ✨ remove unlimited from free items

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const selectors = [
   ':is(div[role="listitem"]):has(:is(.s-result-list .a-spacing-micro))',
   ":is(div[data-asin]):has(:is(.sponsored-brand-label-info-desktop))",
   ':is(div[data-asin]):has(:is([data-is-sponsored-label-active="true"]))',
+  ':is([class*=item-row]):has(:is([src="https://f.media-amazon.com/images/I/11TUGKWP39L.png"]))',
 ];
 
 selectors.forEach((selector) => {
